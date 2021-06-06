6571e6b040e1500523136310811b31a3d3df29b3
paolo <ssaffru@gmail.com>
2021-06-06 12:57:35 +0200

    Update card layout

--- app/src/main/java/com/example/lastminute/MyListAdapter.java
+++ app/src/main/java/com/example/lastminute/MyListAdapter.java
@@ -1,15 +1,12 @@
 package com.example.lastminute;
 
 import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 
 import com.squareup.picasso.Picasso;
 
@@ -35,9 +32,9 @@ public class MyListAdapter extends ArrayAdapter<String> {
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.mylist, null,true);
 
-        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        TextView titleText = (TextView) rowView.findViewById(R.id.tv_title);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-        TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
+        TextView subtitleText = (TextView) rowView.findViewById(R.id.tv_subtitle);
 
         titleText.setText(maintitle[position]);
         Picasso.get().load(imgid[position]).into(imageView);
@@ -45,5 +42,5 @@ public class MyListAdapter extends ArrayAdapter<String> {
 
         return rowView;
 
-    };
+    }
 }

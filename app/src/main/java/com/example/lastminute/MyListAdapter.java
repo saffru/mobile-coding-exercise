0f29d650cabe431594eeb86046aa1051a71e6fd2
Paolo Colombo <ssaffru@gmail.com>
2021-06-06 15:18:15 +0200

    Added element to list

--- app/src/main/java/com/example/lastminute/MyListAdapter.java
+++ app/src/main/java/com/example/lastminute/MyListAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,8 +20,14 @@ public class MyListAdapter extends ArrayAdapter<String> {
     private final String[] maintitle;
     private final String[] subtitle;
     private final String[] imgid;
+    private final String[] ratings;
+    private final String[] prices;
+    private final String[] currencies;
+    private final int[] stars;
 
-    public MyListAdapter(Activity context, String[] maintitle, String[] subtitle, String[] imgid) {
+    public MyListAdapter(Activity context, String[] maintitle,
+                         String[] subtitle, String[] imgid, String[] ratings,
+                         String[] prices, String[] currencies, int[] stars) {
         super(context, R.layout.mylist, maintitle);
         // TODO Auto-generated constructor stub
 
@@ -28,16 +35,22 @@ public class MyListAdapter extends ArrayAdapter<String> {
         this.maintitle=maintitle;
         this.subtitle=subtitle;
         this.imgid=imgid;
-
+        this.ratings=ratings;
+        this.prices=prices;
+        this.currencies=currencies;
+        this.stars=stars;
     }
 
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.mylist, null,true);
 
-        TextView titleText = (TextView) rowView.findViewById(R.id.title);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-        TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
+        TextView titleText = rowView.findViewById(R.id.title);
+        TextView subtitleText = rowView.findViewById(R.id.subtitle);
+        ImageView imageView = rowView.findViewById(R.id.icon);
+        //TextView ratings = rowView.findViewById(R.id.);
+        //TextView price = rowView.findViewById(R.id.tv_price);
+        //LinearLayout ll_stars = rowView.findViewById(R.id.subtitle);
 
         titleText.setText(maintitle[position]);
         Picasso.get().load(imgid[position]).into(imageView);

e983245f3e0bc048dda1fdc0b93d36fe44bfd374
Paolo Colombo <ssaffru@gmail.com>
2021-06-06 22:18:27 +0200

    Implemented order by

--- app/src/main/java/com/example/lastminute/MainActivity.java
+++ app/src/main/java/com/example/lastminute/MainActivity.java
@@ -4,89 +4,178 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ListView list;
+    JSONArray jsonArray;
+    MyListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton ib_order = findViewById(R.id.ib_order);
+        ExpandableLayout expandableLayout =
+                findViewById(R.id.expandable_layout);
 
-        // Instantiate the RequestQueue.
+        list = findViewById(R.id.list);
+
+        ib_order.setOnClickListener(view -> {
+            if (expandableLayout.isExpanded()) {
+                expandableLayout.collapse();
+            } else if (expandableLayout.isExpanded()) {
+                expandableLayout.collapse();
+            } else {
+                expandableLayout.expand();
+            }
+        });
+
+        TextView tv_user_rating = findViewById(R.id.tv_ratings);
+        tv_user_rating.setOnClickListener(view -> {
+            try {
+                jsonArray = getSortedList(jsonArray, "userRating", false);
+                handlingHotels();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+        TextView tv_price_asc = findViewById(R.id.tv_price_asc);
+        tv_price_asc.setOnClickListener(view -> {
+            try {
+                jsonArray = getSortedList(jsonArray, "price", true);
+                handlingHotels();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+        TextView tv_price_desc = findViewById(R.id.tv_price_desc);
+        tv_price_desc.setOnClickListener(view -> {
+            try {
+                jsonArray = getSortedList(jsonArray, "price", false);
+                handlingHotels();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+        TextView tv_stars = findViewById(R.id.tv_stars);
+        tv_stars.setOnClickListener(view -> {
+            try {
+                jsonArray = getSortedList(jsonArray, "stars", false);
+                handlingHotels();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://run.mocky.io/v3/eef3c24d-5bfd-4881-9af7-0b404ce09507";
 
-// Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 response -> {
                     try {
-                        JSONArray jsonArray = new JSONArray(response);
-                        String[] ids = new String[jsonArray.length()];
-                        String[] names = new String[jsonArray.length()];
-                        String[] locations = new String[jsonArray.length()];
-                        int[] stars = new int[jsonArray.length()];
-                        JSONObject checkIns = new JSONObject();
-                        JSONObject checkOuts = new JSONObject();
-                        JSONObject contacts = new JSONObject();
-                        int[] prices = new int[jsonArray.length()];
-                        String[] currencies = new String[jsonArray.length()];
-
-                        String[] ratings = new String[jsonArray.length()];
-                        JSONArray ss []= new JSONArray[10];
-
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            JSONObject hotel = jsonArray.getJSONObject(i);
-                            names[i] = hotel.getString("name");
-                            JSONObject location = hotel.getJSONObject(
-                                    "location");
-                            locations[i] =
-                                    location.getString("address") + ", " + location.getString("city");
-                            stars[i] = hotel.getInt("stars");
-                            checkIns = hotel.getJSONObject(
-                                    "checkIn");
-                            checkOuts = hotel.getJSONObject(
-                                    "checkOut");
-                            contacts = hotel.getJSONObject(
-                                    "contact");
-                            ss[i] = hotel.getJSONArray("gallery");
-                            ratings[i] = hotel.getString("userRating");
-                            prices[i] = hotel.getInt("price");
-                            currencies[i] = hotel.getString("currency");
-                        }
-
-                        MyListAdapter adapter =
-                                new MyListAdapter(MainActivity.this, ids,
-                                        names, locations, ss,
-                                        ratings, prices, currencies,
-                                        stars, checkIns, checkOuts, contacts);
-                        list = findViewById(R.id.list);
-                        list.setAdapter(adapter);
+                        jsonArray = new JSONArray(response);
+                        handlingHotels();
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    // Display the first 500 characters of the response string.
-                    //textView.setText("Response is: "+ response.substring(0,500));
                 }, error -> {
                 });
-
-// Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+    private void handlingHotels() throws JSONException {
+        String[] ids = new String[jsonArray.length()];
+        String[] names = new String[jsonArray.length()];
+        String[] locations = new String[jsonArray.length()];
+        int[] stars = new int[jsonArray.length()];
+        JSONObject checkIns = new JSONObject();
+        JSONObject checkOuts = new JSONObject();
+        JSONObject contacts = new JSONObject();
+        int[] prices = new int[jsonArray.length()];
+        String[] currencies = new String[jsonArray.length()];
+
+        String[] ratings = new String[jsonArray.length()];
+        JSONArray[] images = new JSONArray[10];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject hotel = jsonArray.getJSONObject(i);
+            names[i] = hotel.getString("name");
+            JSONObject location = hotel.getJSONObject(
+                    "location");
+            locations[i] =
+                    location.getString("address") + ", " + location.getString("city");
+            stars[i] = hotel.getInt("stars");
+            checkIns = hotel.getJSONObject(
+                    "checkIn");
+            checkOuts = hotel.getJSONObject(
+                    "checkOut");
+            contacts = hotel.getJSONObject(
+                    "contact");
+            images[i] = hotel.getJSONArray("gallery");
+            ratings[i] = hotel.getString("userRating");
+            prices[i] = hotel.getInt("price");
+            currencies[i] = hotel.getString("currency");
+        }
+
+        adapter =
+                new MyListAdapter(MainActivity.this, ids,
+                        names, locations, images,
+                        ratings, prices, currencies,
+                        stars, checkIns, checkOuts, contacts);
+        list.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    public static JSONArray getSortedList(JSONArray array, String type,
+                                          boolean asc) throws JSONException {
+        List<JSONObject> list = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            list.add(array.getJSONObject(i));
+        }
+        list.sort(new Sort(type, asc));
+        return new JSONArray(list);
+    }
+
+    public static class Sort implements Comparator<JSONObject> {
+        String type;
+        boolean asc;
+        public Sort(String type, boolean asc) {
+            this.type = type;
+            this.asc = asc;
+        }
+        @Override
+        public int compare(JSONObject l, JSONObject r) {
+            try {
+                if(asc)
+                return Integer.compare(l
+                        .getInt(type), r.getInt(type));
+                else
+                    return Integer.compare(r
+                            .getInt(type), l.getInt(type));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return 0;
+
+        }
     }
 }
